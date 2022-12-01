c35d63f28d8a588a2948ca9b976d3100db4b9be5
Ankit <ankitcool4370@gmail.com>
2022-12-01 22:14:37 +0530

    Add new contact to Address Book

---
+++ AddressBookSys/AddressBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_Batch222
+{
+    internal class Addressbook
+    {
+        public static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Entry();
+        }
+    }
+}
+
+
